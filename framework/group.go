b016ac56184c01c0bad66374594343392de639d4
Joker-desire <yangyin1106@163.com>
2023-06-01 18:52:34 +0800

    03｜路由：如何让请求更快寻找到目标函数？
    实现嵌套Group

--- framework/group.go
+++ framework/group.go
@@ -14,35 +14,48 @@ type IGroup interface {
 	Post(string, ControllerHandler)
 	Put(string, ControllerHandler)
 	Delete(string, ControllerHandler)
+
+	// Group 实现嵌套group
+	Group(string) IGroup
 }
 
 type Group struct {
-	core   *Core
-	prefix string
+	core   *Core  //指向core结构
+	parent *Group // 指向上一个Group，如果有的话
+	prefix string //路由前缀
 }
 
 // NewGroup 初始化路由组
 func NewGroup(core *Core, prefix string) *Group {
-	return &Group{core: core, prefix: prefix}
+	return &Group{core: core, parent: nil, prefix: prefix}
 }
 
 func (g *Group) Get(uri string, handler ControllerHandler) {
-	g.core.Get(g.prefix+uri, handler)
+	g.core.Get(g.getAbsolutePrefix()+uri, handler)
 }
 
 func (g *Group) Post(uri string, handler ControllerHandler) {
-	g.core.Get(g.prefix+uri, handler)
+	g.core.Get(g.getAbsolutePrefix()+uri, handler)
 }
 
 func (g *Group) Put(uri string, handler ControllerHandler) {
-	g.core.Get(g.prefix+uri, handler)
+	g.core.Get(g.getAbsolutePrefix()+uri, handler)
 }
 
 func (g *Group) Delete(uri string, handler ControllerHandler) {
-	g.core.Get(g.prefix+uri, handler)
+	g.core.Get(g.getAbsolutePrefix()+uri, handler)
+}
+
+func (g *Group) getAbsolutePrefix() string {
+	if g.parent == nil {
+		return g.prefix
+	}
+	return g.parent.getAbsolutePrefix() + g.prefix
 }
 
 // Group 实现Group方法
-func (c *Core) Group(prefix string) IGroup {
-	return NewGroup(c, prefix)
+func (g *Group) Group(prefix string) IGroup {
+	cGroup := NewGroup(g.core, prefix)
+	cGroup.parent = g
+	return cGroup
 }
